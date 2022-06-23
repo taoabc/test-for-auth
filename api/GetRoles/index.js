337c6d5680bbf6e7061378945526ac5880e3ec80
Haitao Hu <haitaohu@microsoft.com>
2022-06-23 20:06:46 +0800

    test

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -17,7 +17,7 @@ module.exports = async function (context, req) {
   // }
 
   const r = await graphResult(user.accessToken);
-  const roles = ["admin", "authenticated", r, user];
+  const roles = ["admin", "reader"];
 
   context.res.json({
     roles,
