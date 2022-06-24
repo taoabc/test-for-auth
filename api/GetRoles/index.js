0296136604d89e85c592ec8d65e1843b1e24a174
Haitao Hu <haitaohu@microsoft.com>
2022-06-24 10:54:21 +0800

    test

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -2,22 +2,22 @@ const fetch = require("node-fetch").default;
 
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
-  admin: "21a96550-aa02-486e-9297-e6e51b6398fc",
-  reader: "33bb071c-118d-40d1-a5d7-7ced5900b973",
+  admin: "9b090519-6302-4c0d-b747-30e7ecf1770b",
+  reader: "d870939a-0e63-4226-9b85-09b8aa82c521",
 };
 
 module.exports = async function (context, req) {
   const user = req.body || {};
-  //  roles = [];
+  const roles = [];
 
-  // for (const [role, groupId] of Object.entries(roleGroupMappings)) {
-  //     if (await isUserInGroup(groupId, user.accessToken)) {
-  //         roles.push(role);
-  //     }
-  // }
+  for (const [role, groupId] of Object.entries(roleGroupMappings)) {
+    if (await isUserInGroup(groupId, user.accessToken)) {
+      roles.push(role);
+    }
+  }
 
   //   const r = await graphResult(user.accessToken);
-  const roles = ["admin", "reader"];
+  // const roles = ["admin", "reader"];
 
   context.res.json({
     roles,
